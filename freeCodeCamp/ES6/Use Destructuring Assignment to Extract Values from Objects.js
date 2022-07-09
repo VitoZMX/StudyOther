af072ea0e4a5bd14a1c852cb8c7b71827142accb
Виктор Жавнеровский <vi-69@yandex.ru>
2022-07-09 16:53:07 +0300

    feat: freeCodeCamp ES6 func

---
+++ freeCodeCamp/ES6/Use Destructuring Assignment to Extract Values from Objects.js
@@ -0,0 +1,14 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+}
+
+// Only change code below this line
+
+//const today = HIGH_TEMPERATURES.today       // ES5
+//const tomorrow = HIGH_TEMPERATURES.tomorrow // ES5
+
+const { today, tomorrow } = HIGH_TEMPERATURES // ES6
+
+// Only change code above this line
