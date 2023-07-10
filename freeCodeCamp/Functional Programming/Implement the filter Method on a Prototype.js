cd0fb457a9131d4ad2ca793674cb374f7a6be7fa
VitoZMX <Vi-69@yandex.ru>
2023-07-10 16:43:56 +0300

    feat: freeCodeCamp Functional Programming

---
+++ freeCodeCamp/Functional Programming/Implement the filter Method on a Prototype.js
@@ -0,0 +1,15 @@
+// You might learn a lot about the filter method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+
+// Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.
+
+Array.prototype.myFilter = function (callback) {
+    const newArray = []
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+
+    return newArray
+}
